6d9ec58025e636c31d7a42dfde3ec07d11979853
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2023-01-19 02:07:42 +0600

    Modify

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div>
+        <div className="position">
             <nav className='header'>
                 <img src={logo} alt=""></img>
                 <div>
